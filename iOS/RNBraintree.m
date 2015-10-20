c10e537141b8a3d2ee04fd2825974a185d8c3b3e
Alan Wong <alawong@paypal.com>
2015-10-20 17:22:24 +0800

    removed debug logs

--- iOS/RNBraintree.m
+++ iOS/RNBraintree.m
@@ -7,15 +7,11 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(setup:(NSString *)clientToken)
 {
-  
   self.braintree = [Braintree braintreeWithClientToken:clientToken];
-  NSLog(@"Braintree Client initialized.");
-  
 }
 
 RCT_EXPORT_METHOD(showPaymentViewController:(RCTResponseSenderBlock)callback)
 {
-  
   BTDropInViewController *dropInViewController = [self.braintree dropInViewControllerWithDelegate:self];
   dropInViewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(userDidCancelPayment)];
   
@@ -28,24 +24,16 @@ RCT_EXPORT_METHOD(showPaymentViewController:(RCTResponseSenderBlock)callback)
 }
 
 - (void)userDidCancelPayment {
-  
-  NSLog(@"User Cancelled");
   [self.reactRoot dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)dropInViewController:(__unused BTDropInViewController *)viewController didSucceedWithPaymentMethod:(BTPaymentMethod *)paymentMethod {
-  
-  NSLog(@"Payment Success");
   self.callback(@[[NSNull null],paymentMethod.nonce]);
   [viewController dismissViewControllerAnimated:YES completion:nil];
-  
 }
 
 - (void)dropInViewControllerDidCancel:(__unused BTDropInViewController *)viewController {
-  
-  NSLog(@"DropInViewController Cancelled");
-  [viewController dismissViewControllerAnimated:YES completion:nil];
-  
+  [viewController dismissViewControllerAnimated:YES completion:nil];  
 }
 
 @end
